8a4ea717c860d738e5e9942271d91eba2e1a72e3
abivmsul <abdulmaliksultan15@gmail.com>
2025-09-13 17:30:15 +0300

    temp fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,10 @@
 // .eslintrc.js
 module.exports = {
-  root: true,
-  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
-  rules: {
+  "root": true,
+  "extends": ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+  "rules": {
     "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
-      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }
-    ],
+    "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }],
     "@next/next/no-img-element": "warn",
     "jsx-a11y/role-supports-aria-props": "warn"
   }
